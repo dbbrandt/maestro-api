f212b1138fb194b22921b4c2e904e73b01f86c9a
Daniel Brandt <dbbrandt@gmail.com>
2020-01-15 13:54:10 -0500

    fix: Production CORS problem.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000'
+    origins 'localhost:3000','dev.memorymaestro.com:3000', 'www.memorymaestro.com'
 
     resource '*',
       headers: :any,
